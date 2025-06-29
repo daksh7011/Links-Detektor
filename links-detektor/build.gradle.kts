105527324749e52a83361121742d08159a63892b
Daksh <daksh@technowolf.in>
2025-06-29 20:00:58 +0530

    Add environment variables for signing and Maven Central credentials
    - Configure secrets in `master.yml` for publishing to Maven Central.
    - Simplify signing key handling in `build.gradle.kts` using project properties.

--- links-detektor/build.gradle.kts
+++ links-detektor/build.gradle.kts
@@ -177,10 +177,7 @@ publishing {
 
 
 signing {
-    val signingKey = System.getenv("SIGNING_KEY") ?: project.findProperty("signingKey") as String?
-    val signingPassword = System.getenv("SIGNING_PASSWORD") ?: project.findProperty("signingPassword") as String?
-
-    if (signingKey != null && signingPassword != null) {
-        useInMemoryPgpKeys(signingKey, signingPassword)
-    }
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
 }
